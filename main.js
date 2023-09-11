0a9c7acaa3ec27c4d09957480b66f79b0efcbc90
gonrubis00 <juniorubis00@gmail.com>
2023-09-11 18:47:27 -0300

    first commit

---
+++ main.js
@@ -0,0 +1,36 @@
+const express = require('express');
+const app = express();
+const path = require('path');
+
+
+const publicPath = path.resolve(__dirname, './public');
+app.use(express.static(publicPath));
+
+
+//variable de entorno que toma el puerto del servidpr de despliegue con render
+const PORT= process.env.PORT || 8000; //proceso que voy a levantar, node main.js// lee el proceso donde lo levante// o que levante el puerto 8000
+
+app.get('/home', (req, res)=> {
+  //res.send("Hola, estamos en el home")
+  res.sendFile(path.join(__dirname, '/view/home.html'));
+})
+
+let saludo = "Bienvenido/a, ahora estamos en tu perfil"
+
+// Escribir tu código aquí
+app.get('/perfil', (req,res)=> {
+    res.send(saludo)
+})
+
+let producto ={
+  "tipoProducto": null,
+  "precio": null,
+  "cantidad": null
+}
+
+app.get('/producto/agregar', (req,res) => {
+   res.send(producto)
+   
+})
+
+app.listen(PORT, ()=> console.log('Levantando un servidor con ${PORT}'))
